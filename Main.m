76a4d71225fb09438d59f7eabcf116a0586a0539
rachoufbn <rachelle.ef.mail@gmail.com>
2025-08-11 16:48:29 +0300

    Main.m

--- Main.m
+++ Main.m
@@ -27,64 +27,59 @@ end
 
 PlotLinks(Joints, workspace);
 
-% Shape = 2; % 1 for circle, 2 for square
-% 
-% Center = [0,12];
-% radius = 2;
-% n = 100; % number of points
-% 
-% if Shape == 1
-%     [X,Y] = CircleXY(Center, radius, n);
-%     plot(X, Y, '.','Color','b');
-% 
-%     Servo1_Angles = [1,n];
-%     Servo2_Angles = [1,n];
-% 
-%     % InverseKinematics to store computed Servo 1 and 2 angles resp.
-%     for i = 1:n
-%         EndEffector = [X(1,i),Y(1,i)];
-%         [A1, A2] = InverseKinematics(EndEffector,L);
-%         Servo1_Angles(1,i)=A1;
-%         Servo2_Angles(1,i)=A2;
-%     end
-% elseif Shape == 2
-%     SquareEE = SquareXY(Center, radius, n);
-% 
-%     Servo1_Angles = [1,(n+1)];
-%     Servo2_Angles = [1,(n+1)];
-% 
-%     %Plotting Square on Workspace
-%     for i = 1 : n+1
-%         plot(SquareEE(i,1),SquareEE(i,2),'.','Color','b');
-%     end
-%     % InverseKinematics to store computed Servo 1 and 2 angles resp.
-%     for i = 1 : n+1
-%         EndEffector = [SquareEE(i,1),SquareEE(i,2)];
-%         [A1, A2] = InverseKinematics(EndEffector,L);
-%         Servo1_Angles(1,i)=A1;
-%         Servo2_Angles(1,i)=A2;
-%     end
-% end
-% 
-% % Passing Servo Angles to Servo
-% a = arduino('COM5','Mega2560');
-% s1 = servo(a,'D3','MaxPulseDuration',2.5e-3,'MinPulseDuration',0.5e-3);
-% s2 = servo(a,'D5','MaxPulseDuration',2.5e-3,'MinPulseDuration',0.5e-3);
-% 
-% %Converting Between Angle and PWM duty cycle
-% MappedAngles1=[];
-% MappedAngles1=arrayfun(@AngleMapping,Servo1_Angles);
-% MappedAngles2=[];
-% MappedAngles2=arrayfun(@AngleMapping,Servo2_Angles);
-% 
-% % Controlling the Servo Motors
-% for i=1:size(MappedAngles1,2)
-%     writePosition(s1,MappedAngles1(1,i));
-%     writePosition(s2,MappedAngles2(1,i));
-%     pause(0.05);
-% end
-% 
+Shape = 2; % 1 for circle, 2 for square
+ 
+Center = [0,12];
+radius = 2;
+n = 100; % number of points
+ 
+if Shape == 1
+    [X,Y] = CircleXY(Center, radius, n);
+    plot(X, Y, '.','Color','b');
 
+    Servo1_Angles = [1,n];
+    Servo2_Angles = [1,n];
 
+    % InverseKinematics to store computed Servo 1 and 2 angles resp.
+    for i = 1:n
+        EndEffector = [X(1,i),Y(1,i)];
+        [A1, A2] = InverseKinematics(EndEffector,L);
+        Servo1_Angles(1,i)=A1;
+        Servo2_Angles(1,i)=A2;
+    end
+elseif Shape == 2
+    SquareEE = SquareXY(Center, radius, n);
 
+    Servo1_Angles = [1,(n+1)];
+    Servo2_Angles = [1,(n+1)];
 
+    %Plotting Square on Workspace
+    for i = 1 : n+1
+        plot(SquareEE(i,1),SquareEE(i,2),'.','Color','b');
+    end
+    % InverseKinematics to store computed Servo 1 and 2 angles resp.
+    for i = 1 : n+1
+        EndEffector = [SquareEE(i,1),SquareEE(i,2)];
+        [A1, A2] = InverseKinematics(EndEffector,L);
+        Servo1_Angles(1,i)=A1;
+        Servo2_Angles(1,i)=A2;
+    end
+end
+ 
+% Passing Servo Angles to Servo
+a = arduino('COM5','Mega2560');
+s1 = servo(a,'D3','MaxPulseDuration',2.5e-3,'MinPulseDuration',0.5e-3);
+s2 = servo(a,'D5','MaxPulseDuration',2.5e-3,'MinPulseDuration',0.5e-3);
+
+%Converting Between Angle and PWM duty cycle
+MappedAngles1=[];
+MappedAngles1=arrayfun(@AngleMapping,Servo1_Angles);
+MappedAngles2=[];
+MappedAngles2=arrayfun(@AngleMapping,Servo2_Angles);
+
+% Controlling the Servo Motors
+for i=1:size(MappedAngles1,2)
+    writePosition(s1,MappedAngles1(1,i));
+    writePosition(s2,MappedAngles2(1,i));
+    pause(0.05);
+end
